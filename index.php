dbad330b521a5f34aea3e338f6f9b2bbe61f9535
naik3x <86108070+naik3x@users.noreply.github.com>
2021-06-29 21:59:25 +0200

    create frontend

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
     <link rel="stylesheet" href="css/fontawesome.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <title>Just do it!</title>
+    <link rel="stylesheet" href="css/style.css">
 </head>
 
 <body>
@@ -30,26 +31,28 @@
     </nav>
     <br>
     <div class="container">
-        <!--<form>
-            <div class="form-group">
-                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-                <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+        <div class="add-section">
+            <form action="controller/add.php" method="POST" autocomplete="off">
+                <div class="input-group input-group-sm mb-3">
+                    <input type="text" class="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm" placeholder="What do you want to achieve and when?">
+                    <input type="date" class="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm" placeholder="">
+                </div>
+                <div class="input-group input-group-sm mb-3">
+                    <input type="text" class="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm" placeholder="Describe your task here">
+                </div>
+                <button type="button" class="btn btn-secondary btn-sm btn-block bg-dark" style="margin-top: 1%;">Add task &nbsp; &#43;</button>
+            </form>
+        </div>
+        <div class="show-section">
+            <div class="task-item">
+
             </div>
-            <div class="form-group">
-                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="">
-            </div>
-            <div class="form-check">
-                <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                <label class="form-check-label" for="exampleCheck1">Check me out</label>
-            </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
-        </form>-->
+        </div>
     </div>
     <footer class="bg-dark text-center text-white fixed-bottom">
         <!-- Copyright -->
         <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-            © 2021 Technische Berufsschule Zürich:
-            <a class="text-white" href="https://github.com/naik3x/justdoit">Github repository</a>
+            © 2021 Technische Berufsschule Zürich
         </div>
         <!-- Copyright -->
     </footer>
